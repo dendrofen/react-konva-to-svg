6d1e4970f8652fff3cc5231bb573e32dccedc564
dendrofen <ma9k97dev@gmail.com>
2023-09-16 13:17:48 +0300

    feat: added processing status message

--- src/components/ProcessButtons.js
+++ src/components/ProcessButtons.js
@@ -1,16 +1,24 @@
 import styles from '@/app/page.module.css';
 import { exportStageSVG } from "@/lib";
-import { useCallback, useState } from "react";
+import { startTransition, useCallback, useState } from "react";
 
 export default function ProcessButtons() {
     const [image, setImage] = useState();
+    const [isLoading, setLoading] = useState();
 
     const onStageSVG = useCallback(async () => {
+        startTransition(() => {
+            setLoading(true);
+        })
         const svg = await exportStageSVG(window.Konva.stages[0]);
         setImage(svg);
+        setLoading(false);
     }, [setImage])
 
     const onNoImageSVG = useCallback(async () => {
+        startTransition(() => {
+            setLoading(true);
+        })
         const stage = window.Konva.stages[0];
         const imageNode = stage.getLayers()[0].findOne('Image');
         const svg = await exportStageSVG(stage, false, {
@@ -18,10 +26,12 @@ export default function ProcessButtons() {
             onAfter: () => imageNode.show()
         });
         setImage(svg);
+        setLoading(false);
     }, [setImage])
 
     return (
         <>
+            {isLoading && <p>Processing...</p>}
             <div className={styles.grid}>
                 <button type="button" onClick={onStageSVG}>Stage to SVG</button>
                 <button type="button" onClick={onNoImageSVG}>Without Image to SVG</button>
